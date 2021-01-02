a3c1844ca2b73d4cfc475e49bcef2325291d139c
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2021-01-01 19:44:54 -0700

    Nah dude back off
    For when you need someone to back off

---
+++ Nah.py
@@ -0,0 +1,12 @@
+import os
+
+
+
+
+while True:
+    os.system('cmd /k "echo "Nah dude Back off""')
+    os.system('cmd /k "echo "Nah dude Back off""')
+    os.system('cmd /k "echo "Nah dude Back off""')
+    print("Nah Dude back off")
+
+
